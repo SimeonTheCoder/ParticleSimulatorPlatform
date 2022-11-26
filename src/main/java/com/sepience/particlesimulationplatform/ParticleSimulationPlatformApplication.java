c20fe35b28f30d11c9d18beefa5ef216921625f3
Simeon Petkov <petkov.moni@gmail.com>
2022-11-26 23:03:46 +0200

    Uploaded to GitHub!

---
+++ src/main/java/com/sepience/particlesimulationplatform/ParticleSimulationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sepience.particlesimulationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticleSimulationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParticleSimulationPlatformApplication.class, args);
+    }
+
+}
